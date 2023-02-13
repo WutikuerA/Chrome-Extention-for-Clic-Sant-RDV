4406e89c5706a151cee0a72cb325c537ed92eb4e
wutikuer <maltem.WAdilijiang@mezzo.cloud>
2023-02-13 15:29:40 -0500

    functional

--- script.js
+++ script.js
@@ -3,10 +3,33 @@
  var startDate = new Date(); // startDate.toISOString().split('T')[0] to get yyyy-mm-dd
  var endDate = new Date();
  endDate = new Date(endDate.setMonth(endDate.getMonth() + 1));
- var soonestDate = "Loading...";
- var soonestPlace = "Loading...";
+ const LOADING_TEXT = "Loading...";
+ var soonestDate = LOADING_TEXT;
+ var soonestPlace = LOADING_TEXT;
  var bloodTestServiceId;
  console.log("Entered SoonestRDV extention!");
+
+ // MutationObserver -----------------------------------------------
+const observer = new MutationObserver(function (mutations, mutationInstance) {
+    // On postal code search bar displayed
+    var postalCodeSearchBox = document.getElementById("postal-code-stepper");        
+    
+    if (postalCodeSearchBox) 
+    {
+        console.log("postalCodeSearchBox is loaded.");
+        renderInfoPanel();
+        mutationInstance.disconnect();
+    }
+
+});
+
+observer.observe(document, {
+    attributes: true,
+    childList: true,
+    subtree:   true
+});
+
+//-----------------------------------------------------------------------------
 
  // Intercept HTTP response body from clic santé API
  let oldXHROpen = window.XMLHttpRequest.prototype.open;
@@ -20,17 +43,24 @@
 
  async function ProcessHTTPReponseBody(responseBody)
  {
-    var jsonResponse = JSON.parse(responseBody);
-    if(jsonResponse.establishments)
-    {
+     var currentIndex = 0;
+     var jsonResponse = JSON.parse(responseBody);
+     if(jsonResponse.establishments)
+     {
+        document.getElementById("soonest-date").innerHTML = LOADING_TEXT;
+        document.getElementById("soonest-place").innerHTML = LOADING_TEXT;
         console.log(`Found ${jsonResponse.places.length} establishement`);
         for(const place of jsonResponse.places)
         {
             // Get the soonest RDV among the current reponse body
+
             await getSchedules(place);
+            currentIndex++;
+            document.getElementById("loading-text").innerHTML = `(Processing ${currentIndex} / ${jsonResponse.places.length})`;
         }
         console.log("All jobs done!")
-        renderInfoPanel();
+        document.getElementById("soonest-date").innerHTML = soonestDate;
+        document.getElementById("soonest-place").innerHTML = soonestPlace;
     }
 }
 
@@ -40,6 +70,11 @@ async function getSchedules(place)
     bloodTestServiceId = await getServiceId(place);
     soonestDate = await getSoonestDateAndPlace(place, bloodTestServiceId)
     console.log("Leaving Get Schedules"); 
+}
+
+function isALaterThanB(a, b)
+{
+
 }
 
 async function getServiceId(place)
@@ -118,10 +153,14 @@ async function getSoonestDateAndPlace(place, serviceId)
                 var availabilities = data.availabilities;
                 if(availabilities && availabilities.length > 0)
                 {
-                    soonestDate = availabilities[0];
-                    soonestPlace = place.name_fr;
-                    infoPanelText = soonestDate;
-                    console.log(soonestDate); 
+                    // Compare the current date with the previous one
+                    if(soonestDate == LOADING_TEXT || Date.parse(availabilities[0]) < Date.parse(soonestDate))
+                    {
+                        soonestDate =  availabilities[0];
+                        soonestPlace = place.name_fr;
+                        console.log(soonestDate); 
+                    }
+                                        
                 }
                 else
                 {
@@ -133,58 +172,64 @@ async function getSoonestDateAndPlace(place, serviceId)
 }
 
 
-
-
-// MutationObserver -----------------------------------------------
-const observer = new MutationObserver(function (mutations, mutationInstance) {
-    // On postal code search bar displayed
-    var postalCodeSearchBox = document.getElementById("postal-code-stepper");        
-    
-    if (postalCodeSearchBox) 
-    {
-        console.log("postalCodeSearchBox is loaded.");
-        // renderInfoPanel();
-        mutationInstance.disconnect();
-    }
-
-});
-
-observer.observe(document, {
-    attributes: true,
-    childList: true,
-    subtree:   true
-});
-
-//-----------------------------------------------------------------------------
-
-
-function renderInfoPanel(number=null) { 
+function renderInfoPanel() { 
     console.log("Entered renderInfoPanel");
 
     //Info Panel
     var infoPanel = document.createElement( 'div' );
     infoPanel.id = 'soonest-rdv-info-panel';
     infoPanel.style.position = 'fixed';
-    infoPanel.style.top = '20%';
-    infoPanel.style.left = '60%';
-    infoPanel.style.width = '50vw';   
-    infoPanel.style.height = '50px';
-    infoPanel.style.borderRadius = '10px';
-    infoPanel.style.paddingLeft = '20px'
-    infoPanel.style.backgroundColor = 'rgba(0, 217, 255, 0.445)';
+    infoPanel.style.top = '70%';
+    infoPanel.style.right = '0px';
+    infoPanel.style.width = '300px';   
+    infoPanel.style.height = '250px';
+    infoPanel.style.borderRadius = '20px';
+    infoPanel.style.padding = '20px';
+    infoPanel.style.paddingTop = '35px';
+    infoPanel.style.backgroundColor = '#00577dd7'; //'#00577dbb';
     infoPanel.style.display = 'flex';
+    infoPanel.style.flexDirection = 'column';
     infoPanel.style.alignItems = 'center';
     infoPanel.style.zIndex = '9999';
-    infoPanel.textContent = 'Soonest RDV found on: ' + soonestDate + '\n' + 'at: ' + soonestPlace;
-
-        // Panel title
-        // if(number)
-        // {
-        //     var panelTitle = document.createElement( 'div' );
-        //     infoPanel.textContent = `Found ${number} establishments`;
-        //     infoPanel.appendChild( panelTitle );
-        // }
-
+    infoPanel.style.fontFamily = 'sans-serif';    
+    infoPanel.style.color = 'white';
+    // infoPanel.textContent = 'Soonest RDV found on: ' + soonestDate + '\n' + 'at: ' + soonestPlace;
     document.body.appendChild( infoPanel );
+
+    // Panel
+    // var panelIcon = document.createElement("img");
+    // panelIcon.src = chrome.extension.getURL('images/icon_32.png');
+    // infoPanel.appendChild( panelIcon );
+
+
+    var panelTitle = document.createElement( 'div' );
+    panelTitle.style.marginTop = '10px';
+    panelTitle.textContent = "Soonest RDV for blood test:";  
+    panelTitle.style.fontWeight = 'bold';
+    infoPanel.appendChild( panelTitle );
+
+    var dateSpan = document.createElement( 'div' );    
+    dateSpan.id = "loading-text";
+    dateSpan.textContent = '(Processing 0 / 15)';
+    dateSpan.style.textAlign = 'left';
+    dateSpan.style.marginTop = '5px';
+    dateSpan.style.fontSize = 'smaller';
+    dateSpan.style.backgroundColor = 'smaller';
+    infoPanel.appendChild( dateSpan );
+
+    var dateSpan = document.createElement( 'div' );    
+    dateSpan.id = "soonest-date";
+    dateSpan.textContent = soonestDate;
+    dateSpan.style.textAlign = 'left';
+    dateSpan.style.marginTop = '15px';
+    infoPanel.appendChild( dateSpan );
+
+    var palceSpan = document.createElement( 'div' );
+    palceSpan.id = "soonest-place";
+    palceSpan.textContent = soonestPlace;
+    palceSpan.style.marginTop = '15px';
+    palceSpan.style.textAlign = 'left';
+    infoPanel.appendChild( palceSpan );
+
 
 }
